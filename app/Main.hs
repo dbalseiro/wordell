050710561699ac4c4c328cd1069ab9c84a1451c3
Diego Balseiro <dbalseiro@stackbuiilders.com>
2023-08-29 22:29:22 -0500

    Implement first version

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,35 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Main (main) where
 
-import Wordell.Game (game)
+import Wordell.Game
 
-import Polysemy (runFinal)
+import Polysemy
+import Polysemy.State (evalState)
+
+import Data.Function ((&))
+import qualified Data.Text.IO as T
+import qualified Data.Text as T
+
+import System.Random (randomRIO)
 
 main :: IO ()
-main = runFinal game
+main = game
+  & evalState []
+  & randomWordToIOFinal
+  & wordleDisplayToIOFinal
+  & embedToFinal @IO
+  & runFinal
+
+randomWordToIOFinal :: Member (Embed IO) r => Sem (RandomWord ': r) a -> Sem r a
+randomWordToIOFinal = interpret \case
+  PickWord -> embed do
+    dictionary <- T.lines <$> T.readFile "dictionary"
+    n <- randomRIO (0, length dictionary - 1)
+    return (GameWord $ dictionary !! n)
+
+wordleDisplayToIOFinal :: Member (Embed IO) r => Sem (WordleDisplay ': r) a -> Sem r a
+wordleDisplayToIOFinal = interpret \case
+  ReadFromDisplay -> embed T.getLine
+  WriteToDisplay t -> embed (T.putStrLn t)
+
--- src/Wordell/Game.hs
+++ src/Wordell/Game.hs
@@ -1,21 +1,87 @@
-module Wordell.Game (game) where
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
+
+module Wordell.Game (game, GameWord(..), RandomWord(..), WordleDisplay(..)) where
 
 import Data.Text (Text)
+import qualified Data.Text as T
+
+import Control.Monad (when)
+
+import Polysemy
+import Polysemy.State
+import Polysemy.Error
 
 data Outcome = Won | Wrong | OutOfTries
 
-game :: a
-game = do
-  word <- pickWord
-  playTurn word
+newtype GameWord = GameWord Text
+newtype Guess = Guess { unGuess :: Text }
 
-playTurn :: Text -> a
+data RandomWord m a where
+  PickWord :: RandomWord m GameWord
+
+-- pickWord :: Member RandomWord r => Sem r Text
+makeSem ''RandomWord
+
+data WordleDisplay m a where
+  ReadFromDisplay :: WordleDisplay m Text
+  WriteToDisplay :: Text -> WordleDisplay m ()
+
+makeSem ''WordleDisplay
+
+game :: Members '[RandomWord, WordleDisplay, State [Text]] r => Sem r ()
+game = pickWord >>= playTurn
+
+playTurn :: Members '[WordleDisplay, State [Text]] r => GameWord -> Sem r ()
 playTurn word = do
   displayGuesses
   guess <- askGuess
-  outcome <- calculateOutcome word guess
+  outcome <- calculateOutcome guess word
   case outcome of
     Won -> displayWinnerMessage
     OutOfTries -> displayLoserMessage
-    Wrong -> playTurn word
+    Wrong -> do
+      modify (unGuess guess:)
+      playTurn word
+
+askGuess :: Members '[WordleDisplay, State [Text]] r => Sem r Guess
+askGuess = do
+  writeToDisplay "Guess? "
+  s <- readFromDisplay
+  either reask (\() -> return $ Guess s) =<< runError do
+      validateLength s
+      validateState s
+  where
+    reask :: Members '[WordleDisplay, State [Text]] r => Text -> Sem r Guess
+    reask s = do
+      writeToDisplay s
+      askGuess
+
+    validateLength :: Member (Error Text) r => Text -> Sem r ()
+    validateLength s =
+      when (T.length s /= 5) $
+        throw "Only 5-letter words, please"
+
+    validateState :: Members '[Error Text, State [Text]] r => Text -> Sem r ()
+    validateState s = do
+      l <- get
+      when (s `elem` l) $
+        throw "Already tried that"
+
+displayGuesses :: Members '[WordleDisplay, State [Text]] r => Sem r ()
+displayGuesses = get >>= mapM_ writeToDisplay
+
+displayWinnerMessage :: Member WordleDisplay r => Sem r ()
+displayWinnerMessage = writeToDisplay "You won :)"
+
+displayLoserMessage :: Member WordleDisplay r => Sem r ()
+displayLoserMessage = writeToDisplay "Loser!!"
+
+calculateOutcome :: Member (State [Text]) r => Guess -> GameWord -> Sem r Outcome
+calculateOutcome (Guess g) (GameWord w)
+  | g == w = return Won
+  | otherwise = do
+    l <- gets length
+    return $ if l >= 5 then OutOfTries else Wrong
 
